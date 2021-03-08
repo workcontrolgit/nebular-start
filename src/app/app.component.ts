7dd45247e2b659a12e67bb89649023f6c820b87f
Fuji <fuji.nguyen@workcontrol.com>
2021-03-08 00:38:31 -0500

    initial check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NbMenuItem, NbSidebarService} from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nebular-start';
+  items: NbMenuItem[] = [
+    {
+      title: 'Home',
+      icon: 'home-outline',
+      link: '/home',
+      home: true
+    },
+    {
+      title: 'Users',
+      icon: 'people-outline',
+      link: '/users'
+    }
+  ];
+
+  constructor(private readonly sidebarService: NbSidebarService) {
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle();
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule} from '@nebular/theme';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbIconModule,               
+    NbSidebarModule.forRoot(),  
+    NbMenuModule.forRoot(),     
+    NbThemeModule.forRoot({name: 'default'}),    
+    NbLayoutModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
